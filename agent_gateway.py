8c952febf04756e82b32d890904c5d2ed881b361
Santosh Ameti <santosh.ameti@gmail.com>
2024-10-13 22:34:53 -0700

    Agent gateway to handle all variants and initial set of examples to test the gateway

---
+++ agent_gateway.py
@@ -0,0 +1,106 @@
+from typing import Union, List
+from enum import Enum
+from core.abstract_agent import AbstractAgent
+from core.prompt import Prompt
+from core.abstract_tool import Tool
+from core.response import Response, ResponseType
+from utils.agent_logger import AgentLogger
+from adapters.anthropic_claude_agent import AnthropicClaudeAgent
+from adapters.openai_gpt_agent import OpenAIGPTAgent
+from adapters.bedrock_converse_agent import BedrockConverseAgent
+
+class AgentType(Enum):
+    BEDROCK = "bedrock"
+    OPENAI = "openai"
+    VERTEX = "vertex"
+    ANTHROPIC = "anthropic"
+
+class UnsupportedAgentException(Exception):
+    def __init__(self, agent_type):
+        self.agent_type = agent_type
+        super().__init__(f"Unsupported agent type: {agent_type}")
+
+class AgentGateway:
+    def __init__(self, agent_type_or_adapter: Union[AgentType, AbstractAgent], model_id):
+        self.logging = AgentLogger("Agent")
+        self.tools = {}
+        if isinstance(agent_type_or_adapter, AgentType):
+            self.agent_type = agent_type_or_adapter
+            self.adapter = self._get_adapter(agent_type_or_adapter, model_id)
+        elif isinstance(agent_type_or_adapter, AbstractAgent):
+            self.agent_type = None
+            self.adapter = agent_type_or_adapter
+        else:
+            raise UnsupportedAgentException(type(agent_type_or_adapter).__name__)
+
+    def _get_adapter(self, agent_type: AgentType, model_id) -> AbstractAgent:
+        self.logging.info(f"AgentGateway:_get_adapter:Fetching adapter for {agent_type}")
+        if agent_type == AgentType.BEDROCK:
+            self.adapter = BedrockConverseAgent(model_id)
+            return self.adapter
+        elif agent_type == AgentType.OPENAI:
+            self.adapter = OpenAIGPTAgent(model_id)
+            return self.adapter
+        elif agent_type == AgentType.ANTHROPIC:
+            self.adapter = AnthropicClaudeAgent(model_id)
+            return self.adapter
+        elif agent_type == AgentType.VERTEX:
+            pass
+        else:
+            raise UnsupportedAgentException(agent_type.value)
+
+    def prepare_agent(self, prompt: Prompt, tools: list[Tool] = []):
+        self.logging.info(f"AgentGateway:create_agent: Preparing the requested agent")
+        self.adapter.set_instructions(prompt.content)
+
+        for tool in tools:
+            self.logging.info(f"AgentGateway:create_agent: Iterating the tools and validating auth {tool.is_auth_setup()}")
+            if tool.is_auth_setup():
+                self.adapter.add_tool(tool)
+                self.tools[tool.name] = tool
+            else:
+                self.logging.info(
+                    f"AgentGateway:create_agent: Tools {tool.name} auth is not setup, exiting")
+                raise ValueError(f"Invalid auth setup: {tool.name}")
+
+    def run_agent(self, agent_input):
+        final_answer = False
+        tool_response = False
+        self.logging.info(f"AgentGateway:run_agent:Running agent for {agent_input}")
+        while not final_answer:
+
+            response = self.adapter.run(agent_input, is_tool_response=tool_response)
+            self.logging.info(f"AgentGateway:run_agent:Agent execution completed")
+            if response.response_type == ResponseType.ANSWER:
+                self.logging.info(f"AgentGateway:run_agent:Final answer from Agent")
+                final_answer = True
+                tool_response = False
+                return response.content
+            elif response.response_type == ResponseType.TOOL_CALL:
+                self.logging.info(f"AgentGateway:run_agent:Agent responds with tool use")
+                tool_results = []
+                response_tools = response.get_tools()
+                self.logging.info(f"AgentGateway:run_agent: iterating tools")
+
+                for response_tool in response_tools:
+                    self.logging.info(f"AgentGateway:run_agent: executing tool {response_tool.name} and id {response_tool.instance_id}")
+                    if response_tool.name in self.tools:
+                        # get the auth and set the auth to execute
+                        self.logging.info(f"AgentGateway:run_agent: setting the auth params for the agents to execute")
+                        response_tool_name = response_tool.name
+                        response_tool_auth = self.tools[response_tool_name].get_auth()
+                        response_tool.set_auth(**response_tool_auth)
+                        tool_output = response_tool.execute()
+                        formatted_tool_output = self.adapter.get_formatted_tool_output(id=response_tool.instance_id,tool_output=tool_output)
+                        tool_results.append(formatted_tool_output)
+                        self.logging.info(f"AgentGateway:run_agent: {response_tool.get_name()} tool output: {tool_output}")
+                    else:
+                        self.logging.info(f"AgentGateway:run_agent: invalid tool {response_tool.get_name()} returned by Agent")
+                        raise UnsupportedAgentException(f"{response_tool.get_name()} returned by Agent")
+                    self.logging.info(f"AgentGateway:run_agent:Agent execution completed")
+
+                agent_input=tool_results
+                tool_response = True
+            elif response.response_type == ResponseType.ERROR:
+                self.logging.info(f"AgentGateway:run_agent:Agent Errored {response.content}")
+                raise Exception(f"Agent encountered an error: {response.content}")
---
+++ examples/bedrock_weather_translation.py
@@ -0,0 +1,51 @@
+import os
+from dotenv import load_dotenv
+from agent_gateway import AgentGateway, AgentType
+from core.prompt import Prompt
+from tools.weather_tool import WeatherTool
+from tools.translate_tool import TranslationTool
+from tools.askuser_tool import AskUserTool
+
+# Load environment variables
+load_dotenv()
+
+# create the tools needed for the agent
+translation_tool = TranslationTool()
+weather_tool = WeatherTool()
+ask_tool = AskUserTool()
+
+# Set up tool authentication from the .env files
+translation_tool.set_auth(api_key=os.getenv('ANTHROPIC_API_KEY'))
+# Create the agent gateway
+gateway = AgentGateway(AgentType.BEDROCK, model_id="anthropic.claude-3-sonnet-20240229-v1:0")
+
+
+aws_access_key_id = os.getenv('AWS_ACCESS_API')
+aws_secret_access_key = os.getenv('AWS_SECRET_KEY')
+region_name = "us-east-1"
+
+# Set up Anthropic authentication
+
+gateway.adapter.set_auth(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=region_name)
+
+# Set the prompt for Agent with the necessary instructions
+prompt = Prompt("You are a helpful assistant that can translate text and provide weather information. Use the relevant tools as needed, do not respond from memory. When the required answers are available, summarize the response and provide an answer.")
+gateway.prepare_agent(prompt, tools=[translation_tool, weather_tool, ask_tool])
+
+agent_input_1tool = "Translate 'Hello, how are you?' to German"
+agent_input_2tools = "Translate 'Hello, how are you?' to Spanish and tell me the weather in Madrid"
+agent_input_no_params = "Get me weather please"
+multiple_inputs = "Translate to Hindi and Get me weather"
+
+try:
+    result_1 = gateway.run_agent(agent_input_1tool)
+    print(result_1)
+    result_2 = gateway.run_agent(agent_input_2tools)
+    print(result_2)
+    result_3 = gateway.run_agent(agent_input_no_params)
+    print(result_3)
+    result_4 = gateway.run_agent(multiple_inputs)
+    print(f" Agent input: {multiple_inputs}, Agent output: {result_4}")
+except Exception as e:
+    print(str(e))
+
